18dc2246a83edbc0c1ff0c2e8b79085969bc9f5b
SeongHunTed <4047ksh@naver.com>
2022-01-12 01:29:08 +0900

    메뉴별 가격과 Stepper 작동

--- DancingBeans_0106/DancingBeans_0106/OptionViewController.swift
+++ DancingBeans_0106/DancingBeans_0106/OptionViewController.swift
@@ -9,16 +9,30 @@ import UIKit
 
 class OptionViewController: UIViewController{
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    
+    // IBOutlet
     @IBOutlet weak var menuImageTag: UIImageView!
     @IBOutlet weak var menuNameTag: UILabel!
+    @IBOutlet weak var menuValueLabel: UILabel!
+    @IBOutlet weak var menuOrderPrice: UILabel!
+    @IBOutlet var iceHotButtons: [UIButton]!
+    @IBOutlet var shotButtons: [UIButton]!
+
     
+    // var
+    var indexOfOneAndOnly2: Int?
+    var indexOfOneAndOnly: Int?
     var menuName: String?
     var menuImage: UIImage?
+    var menuPrice: Double = 0.0
     
+    
+    // view
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -26,12 +40,22 @@ class OptionViewController: UIViewController{
         menuImageTag.image = menuImage
     }
     
-    @IBOutlet var iceHotButtons: [UIButton]!
-    var indexOfOneAndOnly: Int?
+
+    // 메뉴개수 정하기
+    @IBAction func menuStepper(_ sender: UIStepper) {
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        
+        let price = numberFormatter.string(from: NSNumber(value: sender.value * menuPrice))
+        
+        menuValueLabel.text = Int(sender.value).description
+        menuOrderPrice.text = price! + " 원"
+        
+        
+    }
     
-    @IBOutlet var shotButtons: [UIButton]!
-    var indexOfOneAndOnly2: Int?
-    
+    // 아이스,핫 선택 스위치
     @IBAction func iceHotTouchButtons(_ sender: UIButton){
         if indexOfOneAndOnly != nil {
             if !sender.isSelected{
@@ -51,6 +75,7 @@ class OptionViewController: UIViewController{
         }
     }
     
+    //샷 1샷, 2샷 고르기
     @IBAction func shotTouchButtons(_ sender: UIButton) {
         if indexOfOneAndOnly2 != nil{
             if !sender.isSelected {
@@ -69,6 +94,7 @@ class OptionViewController: UIViewController{
         }
     }
     
+    // 선택한 음료담는 버튼 -> 메인화면으로 이동
     @IBAction func optionPageDone(_ sender: UIButton) {
         
         self.dismiss(animated: true, completion: nil)
--- DancingBeans_0106/DancingBeans_0106/OrderViewController.swift
+++ DancingBeans_0106/DancingBeans_0106/OrderViewController.swift
@@ -12,8 +12,6 @@ class OrderViewController: UIViewController{
 
     @IBOutlet weak var whatthefuck: UIButton!
     
-    
-    
     var menuImageArray =
     [ UIImage(named: "americano"),
       UIImage(named: "Vanilla"),
@@ -57,24 +55,31 @@ class OrderViewController: UIViewController{
         case "Americano":
             nextViewController.menuName = menuArray[0]
             nextViewController.menuImage = menuImageArray[0]
+            nextViewController.menuPrice = 3500
         case "Latte":
             nextViewController.menuName = menuArray[1]
             nextViewController.menuImage = menuImageArray[1]
+            nextViewController.menuPrice = 4000
         case "Vanilla latte":
             nextViewController.menuName = menuArray[2]
             nextViewController.menuImage = menuImageArray[1]
+            nextViewController.menuPrice = 4500
         case "Dancing Latte":
             nextViewController.menuName = menuArray[3]
             nextViewController.menuImage = menuImageArray[2]
+            nextViewController.menuPrice = 5500
         case "Filter":
             nextViewController.menuName = menuArray[4]
             nextViewController.menuImage = menuImageArray[3]
+            nextViewController.menuPrice = 6000
         case "Flat White":
             nextViewController.menuName = menuArray[5]
             nextViewController.menuImage = menuImageArray[4]
+            nextViewController.menuPrice = 4000
         case "Espresso":
             nextViewController.menuName = menuArray[6]
             nextViewController.menuImage = menuImageArray[5]
+            nextViewController.menuPrice = 3000
         default:
             print("error")
         }
