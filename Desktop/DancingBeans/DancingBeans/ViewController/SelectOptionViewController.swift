2c46193fcc17d40a28a690186dd9edd0cca7deba
Jay-Thomas-code <rubat0@naver.com>
2022-02-03 23:20:35 +0900

    UIStepper 를 이용해 메뉴 개수 세기

--- Desktop/DancingBeans/DancingBeans/ViewController/SelectOptionViewController.swift
+++ Desktop/DancingBeans/DancingBeans/ViewController/SelectOptionViewController.swift
@@ -1,72 +1,68 @@
 import UIKit
 
-class SelectOptionViewController: UIViewController, MenuCartDelegate {
+class SelectOptionViewController: UIViewController, PayTableDelegate {
+    
     
     var delegate: SelectOptionDelegate?
     var productName: String = ""
-    var productPrice: String = ""
-    var itemCount: Int = 1
-    
-    var addCount: Int = 0
+    var productPrice: Int = 0
+    var productMenuImage: UIImage? // --- 이전 VC에서 정의할 메뉴 사진
     
     
     @IBOutlet weak var optionMenuNameLabel: UILabel!
     @IBOutlet weak var optionMenuPriceLabel: UILabel!
-     
-    
-    @IBAction func insertCartButtonTapped(_ sender: UIButton) {
-          dismiss(animated: true, completion: nil)
-          delegate?.alarmCartIsFilled(itemCount: itemCount)   // 권한 중에 이런 액션을 실행할거야, 이런 요청을 날릴거야
-      }
+    @IBOutlet weak var menuImageView: UIImageView!
+    @IBOutlet weak var menuCountStepper: UIStepper!
+    @IBOutlet weak var menuCountNumberLabel: UILabel!
+    @IBOutlet weak var menuCountPriceLabel: UILabel!
 
+    
     @IBAction func addMenuCartButtonTapped(_ sender: UIButton) {
-        
-        let MenuCartVC = storyboard?.instantiateViewController(withIdentifier:
-        "MenuCartViewController") as! MenuCartViewController
-        MenuCartVC.delegate = self
-        
-        guard let productName = optionMenuNameLabel.text, let productPrice = optionMenuPriceLabel.text else {
-            return
-        }
-        
-        MenuCartVC.cartMenuName = self.productName
-        MenuCartVC.cartMenuPrice = self.productPrice
-        
-        print("MenuCartVC.cartMenuName : \(MenuCartVC.cartMenuName)")
-        
-        print("MenuCartVC.cartMenuPrice : \(MenuCartVC.cartMenuPrice)")
-        
-        print("before Global Count Int Test : \(Value.sharedInstance().globalCountInt)")
-        
+        // addMenuCartButtonTapped 시 Value file 의 공란의 배열인 globalOrderMenuList 에 self.productName 이 추가된다
+        Value.sharedInstance().globalOrderMenuList.append(self.productName)
+        // addMenuCartButtonTapped 시 Value file 의 0 으로 정의된 정수타입 globalCountInt 가 +1 된다
         Value.sharedInstance().globalCountInt += 1
+     
+        // "선택한 음료 담기" 클릭 시 alert 뜸
+        alarmCartIsFilled(itemCount: Value.sharedInstance().globalCountInt)
+    }
+
+    
+    @IBAction func menuCountNumberStepper(_ sender: UIStepper) {
+        let numberFormatter = NumberFormatter()
+//        numberFormatter.numberStyle = .decimal
+               
+        menuCountNumberLabel.text = Int(sender.value).description
         
-        print("after Global Count Int Test : \(Value.sharedInstance().globalCountInt)")
-        
-        
-        dismiss(animated: true, completion: nil) // -> modal 이었을때
-//        self.navigationController?.popViewController(animated: true)
-        
+        let price = numberFormatter.string(from: NSNumber(value: Int(sender.value)*productPrice))
+
+        menuCountPriceLabel.text = "\(price!) 원"
     }
     
+
     func alarmCartIsFilled(itemCount: Int) {
-        let alertVC = UIAlertController(title: "장바구니 확인", message: "장바구니에 \(itemCount)개의 상품이 추가되었습니다.", preferredStyle: .alert)
+        let alertVC = UIAlertController(title: "장바구니 확인", message: "장바구니에 \(Value.sharedInstance().globalCountInt)개의 상품이 추가되었습니다.", preferredStyle: .alert)
         
-        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        // Parameter {handler} : 사용자가 action 을 취했을 때 일어나는 action
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: { (action) in self.dismiss(animated: true, completion: nil)})
         alertVC.addAction(okAction)
         
         present(alertVC, animated: true, completion: nil)
     }
     
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.optionMenuNameLabel.text = self.productName
-        self.optionMenuPriceLabel.text = self.productPrice
+        optionMenuNameLabel.text = productName
+        optionMenuPriceLabel.text = String(productPrice)
         
-        print("self.productName : \(self.productName)" )
-        
-        // Do any additional setup after loading the view.
-    }
-    
+        menuCountNumberLabel.text = "1"
+        menuCountPriceLabel.text = ("\(productPrice) 원")
+        menuImageView.image = productMenuImage
 
+        menuCountStepper.wraps = true
+        menuCountStepper.autorepeat = true
+        menuCountStepper.minimumValue = 1
+        menuCountStepper.maximumValue = 30
+    }
 }
