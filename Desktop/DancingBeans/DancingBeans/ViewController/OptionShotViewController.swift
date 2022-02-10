0bc5facd32a1e416d5e19a19a33bb0c8764757be
Jay-Thomas-code <rubat0@naver.com>
2022-02-10 23:44:48 +0900

    이전 VC 로 데이터 전달하기

--- Desktop/DancingBeans/DancingBeans/ViewController/OptionShotViewController.swift
+++ Desktop/DancingBeans/DancingBeans/ViewController/OptionShotViewController.swift
@@ -24,28 +24,55 @@ class OptionShotViewController: UIViewController, SelectOptionBottomSheetDelegat
     }
     
     
-    func adjustOption() {   
+    func adjustOption() {
         let SelectOptionBottomSheetVC = storyboard?.instantiateViewController(withIdentifier: "SelectOptionBottomSheetViewController") as! SelectOptionBottomSheetViewController
         SelectOptionBottomSheetVC.delegate = self
-        SelectOptionBottomSheetVC.shotCountLabel = UILabel()
-
-
-        if let test = SelectOptionBottomSheetVC.shotCountLabel {
-            print(SelectOptionBottomSheetVC.shotCountLabel.text)
-//            SelectOptionBottomSheetVC.shotCountLabel.text = esspressoShotCountLabel.text!
-            SelectOptionBottomSheetVC.shotCountLabel.text = "tetetssttt"
-            print("드러온나")
-        } else {
-            print("nono")
-        }
+        
+        
+//        SelectOptionBottomSheetVC.shotCountLabel.text = "program test"
+//
+//        if SelectOptionBottomSheetVC.shotCountLabel.text == nil {
+//            SelectOptionBottomSheetVC.shotCountLabel.text = "넘어가나요"
+//        } else {
+//            SelectOptionBottomSheetVC.shotCountLabel.text = "흠??"
+//        }
+        
+        
+////        if let test = SelectOptionBottomSheetVC.shotCountLabel {
+//            print(SelectOptionBottomSheetVC.shotCountLabel.text)
+////            SelectOptionBottomSheetVC.shotCountLabel.text = esspressoShotCountLabel.text!
+//
+//            print("드러온나")
+//            SelectOptionBottomSheetVC.shotCountLabel.text = "tetetssttt"
+//        } else {
+//            print("nono")
+//        }
         
 //            SelectOptionBottomSheetVC.shotCountLabel.text = esspressoShotCountLabel.text!
-        self.dismiss(animated: true, completion: nil)
+        
+       
+//        self.dismiss(animated: true, completion: nil)
         
     }
-    
+
+    func testAction() {
+//
+//        let vc = self.presentingViewController as? SelectOptionBottomSheetViewController
+//
+//        vc?.shotCount = self.esspressoShotCountLabel.text!
+//
+//        self.presentingViewController?.dismiss(animated: true, completion: nil)
+        
+        let ad = UIApplication.shared.delegate as? AppDelegate
+        
+        ad?.shotCountTest = self.esspressoShotCountLabel.text!
+        
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+        
+    }
     @IBAction func adjustOption(_ sender: Any) {
-        adjustOption()
+//        adjustOption()
+        testAction()
     }
     
     
