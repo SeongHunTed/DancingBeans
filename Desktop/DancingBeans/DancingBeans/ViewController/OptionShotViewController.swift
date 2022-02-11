819c0bd64ac9b83222f4e12bea3863607580001f
Jay-Thomas-code <rubat0@naver.com>
2022-02-11 23:23:16 +0900

    B > A 데이터 전달

--- Desktop/DancingBeans/DancingBeans/ViewController/OptionShotViewController.swift
+++ Desktop/DancingBeans/DancingBeans/ViewController/OptionShotViewController.swift
@@ -8,77 +8,67 @@
 import UIKit
 
 class OptionShotViewController: UIViewController, SelectOptionBottomSheetDelegate {
-    
-//    var delegate: SelectOptionBottomSheetDelegate?
-    
-    func alarmCartIsFilled(itemCount: Int) {
-    }
 
     
     @IBOutlet weak var esspressoShotCountLabel: UILabel!
     @IBOutlet weak var esspressoShotCountStepper: UIStepper!
 
-    
-    @IBAction func countEsspressoShot(_ sender: UIStepper) {
+    @IBAction func countEsspressoShotStepper(_ sender: UIStepper) {
         esspressoShotCountLabel.text = Int(sender.value).description
     }
     
     
     func adjustOption() {
-        let SelectOptionBottomSheetVC = storyboard?.instantiateViewController(withIdentifier: "SelectOptionBottomSheetViewController") as! SelectOptionBottomSheetViewController
-        SelectOptionBottomSheetVC.delegate = self
-        
-        
-//        SelectOptionBottomSheetVC.shotCountLabel.text = "program test"
-//
-//        if SelectOptionBottomSheetVC.shotCountLabel.text == nil {
-//            SelectOptionBottomSheetVC.shotCountLabel.text = "넘어가나요"
-//        } else {
-//            SelectOptionBottomSheetVC.shotCountLabel.text = "흠??"
-//        }
-        
-        
-////        if let test = SelectOptionBottomSheetVC.shotCountLabel {
-//            print(SelectOptionBottomSheetVC.shotCountLabel.text)
-////            SelectOptionBottomSheetVC.shotCountLabel.text = esspressoShotCountLabel.text!
-//
-//            print("드러온나")
-//            SelectOptionBottomSheetVC.shotCountLabel.text = "tetetssttt"
-//        } else {
-//            print("nono")
-//        }
-        
-//            SelectOptionBottomSheetVC.shotCountLabel.text = esspressoShotCountLabel.text!
-        
-       
-//        self.dismiss(animated: true, completion: nil)
-        
     }
-
+    
+    
+    
     func testAction() {
-//
-//        let vc = self.presentingViewController as? SelectOptionBottomSheetViewController
-//
-//        vc?.shotCount = self.esspressoShotCountLabel.text!
-//
-//        self.presentingViewController?.dismiss(animated: true, completion: nil)
-        
         let ad = UIApplication.shared.delegate as? AppDelegate
+
+//        if let first = ad?.shotCountTest {
+//            self.esspressoShotCountLabel.text = first
+//        } else {
+//            print(" nil 임")
+//        }
+//
+        ad?.shotCountTest = self.esspressoShotCountLabel?.text!
         
-        ad?.shotCountTest = self.esspressoShotCountLabel.text!
         
         self.presentingViewController?.dismiss(animated: true, completion: nil)
-        
     }
+    
+    
     @IBAction func adjustOption(_ sender: Any) {
-//        adjustOption()
         testAction()
     }
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let ad = UIApplication.shared.delegate as? AppDelegate
+//
+//        if let first = ad?.shotCountTest {
+//            self.esspressoShotCountLabel.text = first
+//        } else {
+//            print(" viewDidLoad 될 때 nil 임")
+//        }
+        
+        
+        let SelectOptionBottomSheetVC = storyboard?.instantiateViewController(withIdentifier: "SelectOptionBottomSheetViewController") as! SelectOptionBottomSheetViewController
+
+        switch (SelectOptionBottomSheetVC.menuNameLabel?.text) {
+            
+        case "Americano" :
+            print("americano 네")
+        default:
+            print("default")
+        }
+
+        
+        
+        
     }
 
     
